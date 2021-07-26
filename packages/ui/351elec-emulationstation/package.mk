638684fa27b0c82d1b4d7f423b432c7c46c895bb
pkegg <pkegg>
2021-07-26 00:26:24 -0600

    Pull in ES-changes

--- packages/ui/351elec-emulationstation/package.mk
+++ packages/ui/351elec-emulationstation/package.mk
@@ -8,12 +8,16 @@ PKG_GIT_CLONE_BRANCH="main"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="https://github.com/351ELEC/351elec-emulationstation"
+#PKG_SITE="file:///work/351elec-emulationstation"
+#PKG_URL="$PKG_SITE"
+PKG_SITE="https://github.com/pkegg/351elec-emulationstation"
 PKG_URL="$PKG_SITE.git"
 PKG_DEPENDS_TARGET="toolchain SDL2 freetype curl freeimage bash rapidjson ${OPENGLES} SDL2_mixer libcec fping p7zip vlc"
 PKG_NEED_UNPACK="busybox"
 PKG_SHORTDESC="Emulationstation emulator frontend"
 PKG_BUILD_FLAGS="-gold"
+
+#GET_HANDLER_SUPPORT="file"
 GET_HANDLER_SUPPORT="git"
 
 # themes for Emulationstation
